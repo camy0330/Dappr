aef20fdc49a97e07d36e6daec9c4b541990328d6
AliaSham <amiraalia23@gmail.com>
2025-05-31 20:20:05 +0800

    Add files via upload

---
+++ lib/pages/Favourite_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FavouritePage extends StatelessWidget {
+  const FavouritePage({super.key});
+  @override
+  Widget build(BuildContext context) =>
+      const Center(child: Text("This is Favourite Page"));
+}
---
+++ lib/pages/shopping_list_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ShoppingListPage extends StatelessWidget {
+  const ShoppingListPage({super.key});
+  @override
+  Widget build(BuildContext context) =>
+      const Center(child: Text("This is Shopping List Page"));
+}
