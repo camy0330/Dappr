5d7bde494e017ddfd11739b156ef6c75d62b37f6
AliaSham <amiraalia23@gmail.com>
2025-06-10 13:08:52 +0800

    Add files via upload
    Created a new provider to handle user rating functionality for recipes.
    Includes ChangeNotifier setup for state management and methods to update and retrieve ratings.
    This will support future features like displaying average ratings and allowing users to rate recipes.

---
+++ lib/providers/rating_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:dappr/models/rating.dart';
+
+class RatingProvider with ChangeNotifier {
+  final List<Rating> _userRatings = [];
+
+  List<Rating> get userRatings => _userRatings;
+
+  void addRating(Rating rating) {
+    _userRatings.add(rating);
+    notifyListeners();
+  }
+}
